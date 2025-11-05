dc4c471639e3bb98376ac604df0d70bd637d0466
Guilerme-148-Af <guilherme148afonso@gmail.com>
2025-11-05 09:23:10 +0000

    Implement delete cashcard happy path + controller/repo updates

--- src/main/java/example/cashcard/CashCardRepository.java
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
-    CashCard findByIdAndOwner(Long id, String owner);
+    
+    boolean existsByIdAndOwner(Long id, String owner);
 
-    Page<CashCard> findByOwner(String owner, PageRequest pageRequest);
+	CashCard findByIdAndOwner(Long requestedId, String name);
+
+	Page<CashCard> findByOwner(String name, PageRequest of);
+    
 }
